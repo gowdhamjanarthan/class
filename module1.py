9750cce76263c18aa13ebf14944080eb3073b92a
gowdhamjanarthan <167098524+gowdhamjanarthan@users.noreply.github.com>
2024-07-02 18:59:02 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,9 @@
+def displayName(name):
+    print(name)
+
+def addData(a,b):
+    print(a + b)
+
+
+amount = 1300
+
---
+++ moduleImport.py
@@ -0,0 +1,22 @@
+import module1
+import math
+
+module1.displayName("aaaa")
+module1.addData(10,10)
+print(module1.amount)
+
+
+# ceil add one value after . except 0
+price = 2700.80
+print(math.ceil(price))
+
+# floor - should not consider the value after .
+print(math.floor(price))
+
+print(math.factorial(5))
+
+print(math.sqrt(4))
+
+print(math.pi)
+
+print(math.e)
